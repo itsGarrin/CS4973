962bae398bbdd67f9b5d40a9b04a2c1b7db79b7c
itsGarrin <garrin.shieh@gmail.com>
2024-09-12 18:03:18 -0400

    cleaned up code for hw1

--- Homeworks/Homework1/Homework1.py
+++ Homeworks/Homework1/Homework1.py
@@ -1,19 +1,13 @@
 import os
-from openai import OpenAI
+
 from dotenv import load_dotenv
+from openai import OpenAI
+from tqdm.auto import tqdm
 
 load_dotenv()
 
-client = OpenAI(base_url=os.getenv("URL"), api_key=os.getenv("API_KEY"))
+client = OpenAI(base_url=os.getenv("URL"), api_key=os.getenv("KEY"))
 
-resp = client.completions.create(
-    model="Llama3.1-8B-Base",
-    temperature=0.2,
-    max_tokens=100,
-    stop=["\n"],
-    prompt="Shakespeare was",
-)
-print(resp.choices[0].text)
 
 ## Zero-Shot Prompting
 
@@ -22,60 +16,105 @@ from typing import List, Optional
 
 
 def prompt_zero_shot(problem: str) -> str:
-    pass
+    return problem + " Correct Answer: "
 
 
 def extract_zero_shot(completion: str) -> Optional[int]:
-    pass
+    if completion.isdigit():
+        return int(completion)
+
+
+def solve_zero_shot(problem: str) -> Optional[int]:
+    resp = client.completions.create(
+        model="meta-llama/Meta-Llama-3.1-8B",
+        temperature=0.1,
+        prompt=prompt_zero_shot(problem),
+        max_tokens=1
+    )
+
+    return extract_zero_shot(resp.choices[0].text)
 
 
 # Task 2
-def accuracy_zero_shot(problems: List[dict]) -> float:
-    pass
+def accuracy_zero_shot(problems: List[dict]) -> List[float]:
+    accuracies = []
 
+    for _ in tqdm(range(5)):
+        total_correct = 0
 
-# Task 3
-import datasets
+        for item in problems:
+            answer = solve_zero_shot(item["question"])
 
-TEST = datasets.load_dataset("nuprl/llm-systems-math-word-problems", split="test")
+            if answer == item["answer"]:
+                total_correct += 1
+        accuracies.append(total_correct / len(problems))
 
-print(accuracy_zero_shot(TEST))
+    return accuracies
 
 
 ## Few-Shot Prompting
 # Task 4
 def prompt_few_shot(problem: str) -> str:
-    pass
+    return f"""
+    Question: I ate 2 pears and 1 apple. How many fruit did I eat?
+    Answer: 3
+
+    Question: I had ten chocolates but ate one. How many remain?
+    Answer: 9
+
+    Question: {problem}
+    Answer:"""
 
 
 def extract_few_shot(completion: str) -> Optional[int]:
-    pass
+    # only extract digits from str
+    return int("".join(filter(str.isdigit, completion)))
 
 
 def solve_few_shot(problem: str) -> Optional[int]:
-    pass
+    resp = client.completions.create(
+        model="meta-llama/Meta-Llama-3.1-8B",
+        temperature=0.2,
+        prompt=prompt_few_shot(problem),
+        max_tokens=100,
+        stop=["\n"]
+    )
+
+    return extract_few_shot(resp.choices[0].text)
 
 
-def accuracy_few_shot(problems: List[dict]) -> float:
-    pass
+def accuracy_few_shot(problems: List[dict]) -> List[float]:
+    accuracies = []
+
+    for _ in tqdm(range(5)):
+        total_correct = 0
+
+        for item in problems:
+            answer = solve_few_shot(item["question"])
+
+            if answer == item["answer"]:
+                total_correct += 1
+        accuracies.append(total_correct / len(problems))
+
+    return accuracies
 
 
 ## Chain-of-Thought Prompting
 # Task 5
-def prompt_cot(problem: str) -> str:
-    pass
+#def prompt_cot(problem: str) -> str:
+#    pass
 
 
-def extract_few_shot(completion: str) -> Optional[int]:
-    pass
+#def extract_few_shot(completion: str) -> Optional[int]:
+#    pass
 
 
-def solve_few_shot(problem: str) -> Optional[int]:
-    pass
+#def solve_few_shot(problem: str) -> Optional[int]:
+#    pass
 
 
-def accuracy_few_shot(problems: List[dict]) -> float:
-    pass
+#def accuracy_few_shot(problems: List[dict]) -> float:
+#    pass
 
 
 ## Program-Aided Language Models
